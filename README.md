57a6ff2c53440e27baf9ed0fe942e2b961bed29b
TDcoolguy300 <TDcoolguy300@gmail.com>
2024-06-09 10:42:10 -0700

    Fixes, Dripzik, file linking

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
-This is repo for the corrucord mod, a mod for Corru Observer. Expect in-jokes, nonsense, and the like
+This is repo for the corrucord mod, a mod for Corru Observer.
+
+Expect in-jokes, nonsense, and the like
 
 Do not take this as a serious mod.
+
+
+Load in the mod vid the modloader and this URL: https://tdcoolguy300.github.io/corrucordmod/corrucord.js 
--- corrucord.js
+++ corrucord.js
@@ -13,27 +13,28 @@ document.addEventListener('corru_changed', (ev)=>{
 
 // CSS as by Max, removes a lot of my shitcode.
 var css = `
+
 /* BOZKO */
 #realgrid .character.bozko { width: calc(var(--gridTileSize) * 0.8); }
 .bozko figure { transform: translateY(-20%); }
-.bozko .head { background-image: url(https://tdustries.pro/fs/img/obesk/bozkobot/stage/bozkohead.gif) !important; --baseTransform: rotateY(0deg) }
-.bozko .body { background-image: url(https://tdustries.pro/fs/img/obesk/bozkobot/stage/bozkobody.gif) !important; transform: rotateY(180deg) }
-.bozko .arms { background-image: url(https://tdustries.pro/fs/img/obesk/bozkobot/stage/bozkoarms.gif) !important; --baseTransform: rotateY(0deg) }
-    .bozko .arms::after { background-image: url(https://tdustries.pro/fs/img/obesk/bozkobot/stage/bozkofingers.gif) !important; content: ""; }
+.bozko .head { background-image: url(https://tdcoolguy300.github.io/corrucordmod/img/obesk/bozkobot/stage/bozkohead.gif) !important; --baseTransform: rotateY(0deg) }
+.bozko .body { background-image: url(https://tdcoolguy300.github.io/corrucordmod/img/obesk/bozkobot/stage/bozkobody.gif) !important; transform: rotateY(180deg) }
+.bozko .arms { background-image: url(https://tdcoolguy300.github.io/corrucordmod/img/obesk/bozkobot/stage/bozkoarms.gif) !important; --baseTransform: rotateY(0deg) }
+    .bozko .arms::after { background-image: url(https://tdcoolguy300.github.io/corrucordmod/img/obesk/bozkobot/stage/bozkofingers.gif) !important; content: ""; }
 
 body[quality="low"] #content .bozko figure { display: none;}
 body[quality="low"] #content .bozko {
     background-position: top;
     background-image: 
-        url(https://tdustries.pro/fs/img/obesk/bozkobot/stage/bozkohead.gif),
-        url(https://tdustries.pro/fs/img/obesk/bozkobot/stage/bozkobody.gif),
-        url(https://tdustries.pro/fs/img/obesk/bozkobot/stage/bozkoarms.gif),
-        url(https://tdustries.pro/fs/img/obesk/bozkobot/stage/bozkofingers.gif) !important;
+        url(https://tdcoolguy300.github.io/corrucordmod/img/obesk/bozkobot/stage/bozkohead.gif),
+        url(https://tdcoolguy300.github.io/corrucordmod/img/obesk/bozkobot/stage/bozkobody.gif),
+        url(https://tdcoolguy300.github.io/corrucordmod/img/obesk/bozkobot/stage/bozkoarms.gif),
+        url(https://tdcoolguy300.github.io/corrucordmod/img/obesk/bozkobot/stage/bozkofingers.gif) !important;
 }
 
 /* FOCUS APPEARANCE */
 .focus .head.bozko-focus::after {
-    background-image: url(https://tdustries.pro/fs/img/obesk/bozkobot/stage/bozkohead.gif) !important;
+    background-image: url(https://tdcoolguy300.github.io/corrucordmod/img/obesk/bozkobot/full/bozkohead.gif) !important;
     background-size: auto 285%;
     background-position: center top;
     transform: 60vmin;
@@ -42,14 +43,74 @@ body[quality="low"] #content .bozko {
 
 /* VN */
 #bozko.vn-char { height: 150vmin }
-#bozko.vn-char .head { background-image: url(https://tdustries.pro/fs/img/obesk/bozkobot/stage/bozkohead.gif) !important;}
-    .vn-wrapper.vn-con #bozko .head { background-image: url(/img/sprites/obesk/bozko/full/connector.gif), https://tdustries.pro/fs/img/obesk/bozkobot/stage/bozkohead.gif) !important;}
-#bozko.vn-char .body { background-image: url(https://tdustries.pro/fs/img/obesk/bozkobot/stage/bozkobody.gif) !important;}
-#bozko.vn-char .arms { background-image: url(https://tdustries.pro/fs/img/obesk/bozkobot/stage/bozkoarms.gif) !important;}
-    #bozko.vn-char .arms::before { background-image: url(https://tdustries.pro/fs/img/obesk/bozkobot/stage/bozkofingers.gif) !important; content: ""; background-position: bottom;}
+#bozko.vn-char .head { background-image: url(https://tdcoolguy300.github.io/corrucordmod/img/obesk/bozkobot/full/bozkohead.gif) !important;}
+    .vn-wrapper.vn-con #bozko .head { background-image: url(/img/sprites/obesk/bozko/full/connector.gif), https://tdcoolguy300.github.io/corrucordmod/img/obesk/bozkobot/full/bozkohead.gif) !important;}
+#bozko.vn-char .body { background-image: url(https://tdcoolguy300.github.io/corrucordmod/img/obesk/bozkobot/full/bozkobody.gif) !important;}
+#bozko.vn-char .arms { background-image: url(https://tdcoolguy300.github.io/corrucordmod/img/obesk/bozkobot/full/bozkoarms.gif) !important;}
+    #bozko.vn-char .arms::before { background-image: url(https://tdcoolguy300.github.io/corrucordmod/img/obesk/bozkobot/full/bozkofingers.gif) !important; content: ""; background-position: bottom;}
 
+
+
+
+
+
+
+/* TOZIK */
+.tozik .body {background-image: url(https://tdcoolguy300.github.io/corrucordmod/img/obesk/tozikdrip/stage/tozikjacket.gif), url(/img/sprites/obesk/tozik/stage/tozik_body.gif);}
+.tozik .body::after {content: ""; background-image: url(https://tdcoolguy300.github.io/corrucordmod/img/obesk/tozikdrip/stage/toziksleeves.gif), url(/img/sprites/obesk/tozik/stage/tozik_arms.gif);}
+.tozik .head { background-image: url(/img/sprites/obesk/tozik/stage/tozik_eyes.gif), url(https://tdcoolguy300.github.io/corrucordmod/img/sprites/obesk/tozikdrip/stage/tozik_head.gif), url(https://tdcoolguy300.github.io/corrucordmod/img/sprites/obesk/tozikdrip/stage/tozik_headcore.gif); }
+#realgrid[stage="embassy_research"] .character.tozik {
+    --charTransform: translateZ(calc(var(--gridTileSize) * -0.9)) translateY(10%);
+}
+
+.collapse #realgrid .truecreature .tozik .head {
+    background-image: url(/img/sprites/obesk/tozik/stage/connector.gif), url(/img/sprites/obesk/tozik/stage/tozik_eyes.gif), url(https://tdcoolguy300.github.io/corrucordmod/img/sprites/obesk/tozikdrip/stage/tozik_head.gif), url(https://tdcoolguy300.github.io/corrucordmod/img/sprites/obesk/tozikdrip/stage/tozik_headcore.gif);
+}
+
+body[quality="low"] #content .tozik figure { display: none;}
+body[quality="low"] #content .tozik {
+    background-image: 
+        url(/img/sprites/obesk/tozik/stage/tozik_eyes.gif),
+        url(https://tdcoolguy300.github.io/corrucordmod/img/sprites/obesk/tozikdrip/stage/tozik_head.gif),
+        url(https://tdcoolguy300.github.io/corrucordmod/img/sprites/obesk/tozikdrip/stage/tozik_headcore.gif),
+		url(https://tdcoolguy300.github.io/corrucordmod/img/sprites/obesk/tozikdrip/stage/toziksleeves.gif),
+        url(/img/sprites/obesk/tozik/stage/tozik_arms.gif),
+		url(https://tdcoolguy300.github.io/corrucordmod/img/sprites/obesk/tozikdrip/stage/tozikjacket.gif),
+        url(/img/sprites/obesk/tozik/stage/tozik_body.gif)
+}
+/* VN */
+.vn-char#tozik {
+    height: 180vmin; //Before you ask, i edited the full ones only, not the stage ones.
+}
+.vn-char#tozik .head { background-image: url(/img/sprites/obesk/tozik/full/tozik_eyes.gif), url(https://tdcoolguy300.github.io/corrucordmod/img/sprites/obesk/tozikdrip/full/tozik_head.gif), url(https://tdcoolguy300.github.io/corrucordmod/img/sprites/obesk/tozikdrip/full/tozik_headcore.gif); }
+.vn-char#tozik .body { background-image: url(https://tdcoolguy300.github.io/corrucordmod/img/sprites/obesk/tozikdrip/full/tozikjacket.gif), url(/img/sprites/obesk/tozik/full/tozik_body.gif); }
+.vn-char#tozik .body::before {
+    background-image: url(https://tdcoolguy300.github.io/corrucordmod/img/sprites/obesk/tozikdrip/full/toziksleeves.gif), url(/img/sprites/obesk/tozik/full/tozik_arms.gif);
+    background-size: inherit;
+    content: "";
+    animation: TINYHOVER 10s EASE-IN-OUT infinite alternate;
+}
+.vn-wrapper.vn-con #tozik .head { background-image: url(/img/sprites/obesk/tozik/full/connector.gif), url(/img/sprites/obesk/tozik/full/tozik_eyes.gif), url(https://tdcoolguy300.github.io/corrucordmod/img/sprites/obesk/tozikdrip/full/tozik_head.gif), url(https://tdcoolguy300.github.io/corrucordmod/img/sprites/obesk/tozikdrip/full/tozik_headcore.gif); }
+#content[embassyday="1"] .vn-char#tozik .head { background-image: url(/img/sprites/obesk/tozik/full/connectoralt.gif), url(/img/sprites/obesk/tozik/full/tozik_eyes.gif), url(https://tdcoolguy300.github.io/corrucordmod/img/sprites/obesk/tozikdrip/full/tozik_head.gif), url(https://tdcoolguy300.github.io/corrucordmod/img/sprites/obesk/tozikdrip/full/tozik_headcore.gif); }
+
+/* FOCUS */
+.focus .head.tozik-focus {
+    --rotation: rotateX(-90deg) rotateY(90deg);
+    transform: translate(-100%, 0) var(--rotation);
+    background-image: url(/img/sprites/obesk/tozik/full/tozik_eyes.gif), url(https://tdcoolguy300.github.io/corrucordmod/img/sprites/obesk/tozikdrip/full/tozik_head.gif), url(https://tdcoolguy300.github.io/corrucordmod/img/sprites/obesk/tozikdrip/full/tozik_headcore.gif);
+    background-size: auto 365%;
+    background-position: center 0;
+}
+
+
+
+
+
+
+
+/* LAMP */
 #realgrid .lamp figure::before, #realgrid .lamp figure::after {
-    background-image: url(https://tdustries.pro/fs/img/things/lamp.gif) !important;
+    background-image: url(https://tdcoolguy300.github.io/corrucordmod/img/things/lamp.gif) !important;
 }
 
 
@@ -68,7 +129,7 @@ document.head.appendChild(document.createElement('style').appendChild(document.c
 	
 function updateEntityNames(){
 		copyEntity('bozko','notsobozko')
-		env.entities.notsobozko.image = "https://tdustries.pro/fs/img/obesk/bozkobot/portrait.gif"
+		env.entities.notsobozko.image = "https://tdcoolguy300.github.io/corrucordmod/img/obesk/bozkobot/portrait.gif"
 		env.entities.notsobozko.text =`
 			::RESPONSIVE THOUGHTFORM
 			::EXPLICIT PURPOSE::'recollection'
@@ -100,7 +161,7 @@ function copyEntity(oldEnt,newEnt){ //Different to the baseline createEntity, th
 
 function updateDialogActors(){
 	env.dialogueActors.bozko.name = "NotSoBozko"
-	env.dialogueActors.bozko.image = "'https://tdustries.pro/fs/img/obesk/bozkobot/portrait.gif'"
+	env.dialogueActors.bozko.image = "'https://tdcoolguy300.github.io/corrucordmod/img/obesk/bozkobot/portrait.gif'"
 	
 }
 
@@ -140,15 +201,15 @@ function updateDialogActors(){
 /*
 
 function initCharSprites(){ // Sometimes people go missing, we'll reinit here for them.
-	changeSpriteOfTargetStage('.bozko .head', 'https://tdustries.pro/fs/img/obesk/bozkobot/stage/bozkohead.gif')
-	changeSpriteOfTargetStage('.bozko .arms', 'https://tdustries.pro/fs/img/obesk/bozkobot/stage/bozkoarms.gif')
-	changeSpriteOfTargetStage('.bozko .body', 'https://tdustries.pro/fs/img/obesk/bozkobot/stage/bozkobody.gif')
+	changeSpriteOfTargetStage('.bozko .head', 'https://tdcoolguy300.github.io/corrucordmod/img/obesk/bozkobot/stage/bozkohead.gif')
+	changeSpriteOfTargetStage('.bozko .arms', 'https://tdcoolguy300.github.io/corrucordmod/img/obesk/bozkobot/stage/bozkoarms.gif')
+	changeSpriteOfTargetStage('.bozko .body', 'https://tdcoolguy300.github.io/corrucordmod/img/obesk/bozkobot/stage/bozkobody.gif')
 	changeSpriteOfTargetVN('bozko', 
 				`
 	                <div id="bozko" class="vn-char vn-qou">
-                    <div class="headbot" style="background-image: url('https://tdustries.pro/fs/img/obesk/bozkobot/full/bozkohead.gif');"></div> 
-					<div class="bodybot" style="background-image: url('https://tdustries.pro/fs/img/obesk/bozkobot/full/bozkobody.gif');"></div> 
-					<div class="armsbot" style="background-image: url('https://tdustries.pro/fs/img/obesk/bozkobot/full/bozkoarms.gif');"></div>
+                    <div class="headbot" style="background-image: url('https://tdcoolguy300.github.io/corrucordmod/img/obesk/bozkobot/full/bozkohead.gif');"></div> 
+					<div class="bodybot" style="background-image: url('https://tdcoolguy300.github.io/corrucordmod/img/obesk/bozkobot/full/bozkobody.gif');"></div> 
+					<div class="armsbot" style="background-image: url('https://tdcoolguy300.github.io/corrucordmod/img/obesk/bozkobot/full/bozkoarms.gif');"></div>
                     <span class="target" entity="bozko"></span>
                 </div>`
 				)
@@ -159,7 +220,7 @@ function initCharSprites(){ // Sometimes people go missing, we'll reinit here fo
 function
 function updateDialogActors(){
 	env.dialogueActors.bozko.name = "NotSoBozko"
-	env.dialogueActors.bozko.image = "'https://tdustries.pro/fs/img/obesk/bozkobot/portrait.gif'"
+	env.dialogueActors.bozko.image = "'https://tdcoolguy300.github.io/corrucordmod/img/obesk/bozkobot/portrait.gif'"
 	
 }
 
